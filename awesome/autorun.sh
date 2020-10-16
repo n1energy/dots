0394fe3551918946a7e67c93419f13a9c4c83c92
piwi <qwe@mail.ru>
2020-10-16 15:53:59 +1000

    first

---
+++ awesome/autorun.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ; then
+    $@&
+  fi
+}
+run nitrogen --restore
+run nm-applet
+run dbus-launch update-checker
+run light-locker
+run pulseaudio -D
+run compton --shadow-exclude '!focused'
+run xcape -e 'Super_L=Super_L|Shift_L|p'
+run /usr/lib/mate-polkit/polkit-mate-authentication-agent-1
+run thunar --daemon
+run xfce4-power-manager
+run xrdb merge ~/.Xresources
+run xfsettingsd
+run gnome-keyring-daemon
+run setxkbmap -layout "us,ru" -option "grp:alt_shift_toggle"
+run redshift
+run volumeicon
+run clipit
+execute_script("document.evaluate
